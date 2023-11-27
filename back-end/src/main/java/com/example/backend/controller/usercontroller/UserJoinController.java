769960696104969e3098be06d84918545f463a3c
YSJ <yu9738@naver.com>
2023-11-27 18:42:47 +0900

    로그인, 회원가입, 회원탈퇴

--- back-end/src/main/java/com/example/backend/controller/usercontroller/UserJoinController.java
+++ back-end/src/main/java/com/example/backend/controller/usercontroller/UserJoinController.java
@@ -10,21 +10,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class UserController implements Controller {
+public class UserJoinController implements Controller {
     UserDao userDao =new UserDao();
     @Override
     public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        User user = userDao.findByID(1);
-
+        User user = new User();
+        
         // 응답 헤더 설정
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-
-
+        
+        //user 매핑
+        user.setUserId(request.getParameter("id"));
+        user.setPassword(request.getParameter("password"));
+        user.setName(request.getParameter("name"));
+        user.setPhoneNum(request.getParameter("phoneNumber"));
+        user.setNickname(request.getParameter("nickname"));
+        user.setSchool_id(Integer.valueOf(request.getParameter("school_id")));
+        user.setMajor_id(Integer.valueOf(request.getParameter("major_id")));
+        
+        //insert 성공시 1 , 실패시 0
+        int querySuccessCheck = userDao.join(user);
         ObjectMapper mapper = new ObjectMapper();
         String userJson = mapper.writeValueAsString(user);
-
-        // 응답 데이터 전송
-        response.getWriter().write(userJson);
+        
+        //Json 으로 리턴
+        response.getWriter().write("{\"querySuccessCheck\" : \"" + querySuccessCheck + "\"}");
     }
 }
